067279ff629f9c4caa1a2b453a8b662df3cd97c5
Ruqoyah <rukayatodukoya123@gmail.com>
2017-01-09 16:18:55 +0100

    mocha Tests updated

---
+++ tests/getPrimesTests.js
@@ -0,0 +1,46 @@
+'use strict'
+
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+var myApp = require('../app/getPrimesLab.js');
+
+describe("get prime test", function() {
+
+    it('should return [2,3] for [2, 3, 5 , 7]', function () {
+      expect(myApp.getprimes([1, 2, 3, 4])).to.equal([1, 4]);
+    });
+
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+    it('should return [3,5] for [3, 5, 7 , 11]', function () {
+      expect(myApp.getprimes([3, 5, 7, 11])).to.equal([1, 4]);
+    });
+
+ });
+
+  
